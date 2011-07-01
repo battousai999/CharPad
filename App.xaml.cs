51c043daf617092c3e138e25288202742ff46cc1
kjones <battousai999@gmail.com>
2011-07-01 13:30:08 -0600

    Fixed a textbox focus text selection problem.

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace CharPad
 {
@@ -12,5 +13,14 @@ namespace CharPad
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // This causes textbox to select all text when tabbing into them.
+            EventManager.RegisterClassHandler(typeof(TextBox),
+                    TextBox.GotFocusEvent,
+                    new RoutedEventHandler((sender, args) => { (sender as TextBox).SelectAll(); }));
+
+            base.OnStartup(e);
+        }
     }
 }
